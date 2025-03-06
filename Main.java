1f8c0027deeb597ae077f9a07c9b26428e19949e
RubyD <143292762+ohnoruu@users.noreply.github.com>
2025-03-06 18:59:21 +0000

    yay

--- Main.java
+++ Main.java
@@ -4,17 +4,31 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+    int [][] matrix1 = { {8,7,5,8}, {1,3,9,5}, {3,4,6,2}};
+    System.out.println(sumOfDiag(matrix1));
+    System.out.println(productTable(6,5));
   }
 
 
   public static int sumOfDiag(int[][] matrix)
   {
-    return 0;
+    int sum=0;
+    for (int i=0; i<matrix.length; i++){
+      sum+=matrix[i][i];
+    }
+    return sum;
   }
 
   public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    int [][] table = new int[numRows][numCols];
+    for (int row=0; row<numRows; row++){
+      for (int col=0; col<numCols; col++){
+        table[row][col] = row*col;
+        System.out.print(row*col);
+      }
+      System.out.println();
+    }
+    return table;
   }
 }
